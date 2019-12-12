3e2af1e75d8c769eb23c69727189888004e9de55
Teodor Cucu <teodorcucu99@gmail.com>
2019-12-12 14:33:58 +0100

    Update d_upgrade.sh

--- d_upgrade.sh
+++ d_upgrade.sh
@@ -8,37 +8,37 @@ NOCOLOR="\033[0m"
 echo
 
 echo -e "step 1: ${GREEN}pre-configuring packages${NOCOLOR}"
-sudo dpkg --configure -a -y
+sudo dpkg --configure -a 
 
 echo
 
 echo -e "step 2: ${GREEN}fix and attempt to correct a system with broken dependencies${NOCOLOR}"
-sudo apt-get install -f -y
+sudo apt-get install -f
 
 
 echo
 
 echo -e "step 3: ${GREEN}update apt cache${NOCOLOR}"
-sudo apt-get update -y
+sudo apt-get update
 
 echo
 
 echo -e "step 4: ${GREEN}upgrade packages${NOCOLOR}"
-sudo apt-get upgrade -y
+sudo apt-get upgrade
 
 echo
 
 echo -e "step 5: ${GREEN}distribution upgrade${NOCOLOR}"
-sudo apt-get dist-upgrade -y
+sudo apt-get dist-upgrade
 
 echo
 
 echo -e "step 6: ${GREEN}remove unused packages${NOCOLOR}"
-sudo apt-get --purge autoremove -y
+sudo apt-get --purge autoremove
 
 echo
 
 echo -e "step 7: ${GREEN}clean up${NOCOLOR}"
-sudo apt-get autoclean -y
+sudo apt-get autoclean 
 
 echo
